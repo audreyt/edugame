279c965106ca8a6432294c533cf3dd668784db9e
Audrey Tang <audreyt@audreyt.org>
2011-03-28 00:03:35 +0800

    * Finish parsing lessons, completing all card types.

--- v2.hs
+++ v2.hs
@@ -11,8 +11,8 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Attoparsec.Text as P
 
-_Cards_ = [] --students
 
+type Power = Maybe Int
 data Style = V | A | R | K | Anti Style deriving Show
 
 -- 「學門」：遊戲有「數學」、「中文」、「英文」、「自然」、「社會」、「藝術」、「健體」七個學門。
@@ -24,12 +24,41 @@ c = Chi; e = Eng; m = Mat; n = Nat; s = Soc; a = Art; p = Phy
 
 data Card
     = Student -- 學生
-        { serial            :: Int          -- 序號
+        { serial            :: Int        -- 序號
         , name              :: Text       -- 名稱
-        , styles            :: [Style]      -- 學習風格
-        , threshold         :: Int          -- 蒙昧值
-        , interested        :: [Topic]      -- 有興趣之學門
-        , paralyzed         :: [Topic]      -- 有麻痺之學門
+        , styles            :: [Style]    -- 學習風格
+        , threshold         :: Int        -- 蒙昧值
+        , interests         :: [Topic]    -- 有興趣之學門
+        , paralyses         :: [Topic]    -- 有麻痺之學門
+        , flavor            :: Text       -- 斜體字
+        }
+    | Action -- 行動
+        { serial            :: Int        -- 序號
+        , name              :: Text       -- 名稱
+        , turns             :: Int        -- 所需回合
+        , effect            :: Text       -- 效果
+        , flavor            :: Text       -- 斜體字
+        }
+    | Environment -- 環境
+        { serial            :: Int        -- 序號
+        , name              :: Text       -- 名稱
+        , effect            :: Text       -- 效果
+        , flavor            :: Text       -- 斜體字
+        }
+    | Skill -- 技藝
+        { serial            :: Int        -- 序號
+        , name              :: Text       -- 名稱
+        , effect            :: Text       -- 效果
+        , flavor            :: Text       -- 斜體字
+        }
+    | Lesson -- 教學
+        { serial            :: Int        -- 序號
+        , name              :: Text       -- 名稱
+        , styles            :: [Style]    -- 學習風格
+        , interested        :: Maybe Int  -- 成就點數(有興趣時)
+        , uninterested      :: Maybe Int  -- 成就點數(無興趣時)
+        , topics            :: [Topic]    -- 學門
+        , abilities         :: [Ability]  -- 特殊能力
         , flavor            :: Text       -- 斜體字
         }
     deriving Show
@@ -42,6 +71,7 @@ say = putStrLn
 _Left_ = 9.6
 _Top_ = 13.65
 
+{-
 _main = do
     say [qq|
 
@@ -55,6 +85,7 @@ tell application "OmniGraffle Professional 5"
     end tell
 end tell
     |]
+-}
 
 
 type X = Float
@@ -62,10 +93,13 @@ type Y = Float
 renderCards :: X -> Y -> [Card] -> String
 renderCards = undefined
 
+-- 「特殊」：有些牌有特殊能力，如解麻痺、引發興趣等。
+data Ability = Unparalyze | Inspire deriving Show
+
 parseTable :: Text -> [[(Text, Text)]]
-parseTable s = [hs `zip` proc b | b <- tbody]
+parseTable s = [hs `zip` proc b | b <- tbody, not (T.null b) ]
     where
-    (thead:tbody) = T.lines s
+    (thead:tbody) = map T.strip $ T.lines s
     proc = tail . map T.strip . T.splitOn "|"
     hs = proc thead
 
@@ -86,13 +120,11 @@ instance ShowQ [Row] where
 -- say [qq| { head $ parseTable s } |]
 main = do
     students     <- parseStudent `from` "students"
-    {-
     actions      <- parseAction `from` "actions"
     environments <- parseEnvironment `from` "environments"
-    lessons      <- parseLesson `from` "lessons"
     skills       <- parseSkill `from` "skills"
-    -}
-    print students
+    lessons      <- parseLesson `from` "lessons"
+    let _Cards_ = students ++ actions ++ environments ++ skills ++ lessons in print _Cards_
 
 parser `from` table = do
     f <- T.readFile $ __Bin__ ++ "/v2/" ++ table ++ ".txt"
@@ -104,18 +136,48 @@ row <<< label = case lookup label row of
     _         -> error $ show (row, label)
 
 class Grok a where
+    grok :: Text -> a
+    grok = runParser parser
     parser :: Parser a
-
-grok :: Grok a => Text -> a
-grok = runParser parser
+    parser = error "Parser undefined"
 
 instance Grok String where parser = fmap T.unpack $ P.takeWhile (const True)
 instance Grok Text where parser = P.takeWhile (const True)
 instance Grok Int where parser = decimal
-instance Grok (Style -> Maybe Style)
-    where parser = (char 'a' *> pure Just)
-             <|> (char 'x' *> pure (Just . Anti))
-             <|> (pure $ const Nothing)
+instance Grok (Maybe Int) where parser = fmap Just decimal <|> pure Nothing
+instance Grok (Style -> Maybe Style) where
+    parser = (char 'a' *> pure Just)
+         <|> (char 'o' *> pure Just)
+         <|> (char 'x' *> pure (Just . Anti))
+         <|> (pure $ const Nothing)
+
+instance Grok [Ability] where
+    grok text = catMaybes $ map parsePair abilityMap
+        where
+        parsePair (ability, ch) = fmap (const ability) $ T.find (== ch) text
+
+
+parseAction :: Row -> Card
+parseAction r = Action
+    { serial     = r <<< "編號"
+    , name       = r <<< "名稱"
+    , turns      = r <<< "所需行動"
+    , effect     = r <<< "效果"
+    , flavor     = r <<< "斜體字"
+    }
+
+parseSkill :: Row -> Card
+parseSkill r = Skill
+    { serial     = r <<< "編號"
+    , name       = r <<< "名稱"
+    , effect     = r <<< "效果"
+    , flavor     = r <<< "斜體字"
+    }
+
+parseEnvironment :: Row -> Card
+parseEnvironment r = case parseSkill r of
+    Skill s n e f  -> Environment s n e f
+    _              -> undefined
 
 parseStudent :: Row -> Card
 parseStudent r = Student
@@ -124,12 +186,26 @@ parseStudent r = Student
     , threshold  = r <<< "蒙昧值"
     , flavor     = r <<< "斜體字"
     , styles     = parseStyles r styleMap
-    , interested = parseTopics r 'a' topicMap
-    , paralyzed  = parseTopics r 'x' topicMap
+    , interests  = parseTopics r 'a' topicMap
+    , paralyses  = parseTopics r 'x' topicMap
     }
-    where
-    styleMap = [(V, "視"), (A, "聽"), (R, "讀"), (K, "作")]
-    topicMap = [(c, "文學"), (e, "外語"), (m, "數學"), (n, "自然"), (s, "社會"), (a, "藝術"), (p, "健體")]
+
+-- | 編號 | 名稱 | V | A | R | K | 教學風格 | 興趣力道 | 無興趣力道 | 學科 | 文學 | 外語 | 數學 | 自然 | 社會 | 藝術 | 健體 | 特殊效果 | 斜體字 |
+parseLesson :: Row -> Card
+parseLesson r = Lesson
+    { serial       = r <<< "編號"
+    , name         = r <<< "名稱"
+    , flavor       = r <<< "斜體字"
+    , styles       = parseStyles r styleMap
+    , topics       = parseTopics r 'o' topicMap
+    , interested   = r <<< "興趣力道"
+    , uninterested = r <<< "無興趣力道"
+    , abilities    = r <<< "特殊效果"
+    }
+
+styleMap = [(V, "視"), (A, "聽"), (R, "讀"), (K, "作")]
+topicMap = [(c, "文學"), (e, "外語"), (m, "數學"), (n, "自然"), (s, "社會"), (a, "藝術"), (p, "健體")]
+abilityMap = [(Inspire, 'I'), (Unparalyze, 'U')]
 
 parseStyles r pairs = catMaybes $ map parsePair pairs
     where
